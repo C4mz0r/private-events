65a17635ec1c0d5eb74d37066f4d566ebc62b52d
C4mz0r <aut0x@hotmail.com>
2016-07-03 22:27:19 -0600

    User show page lists the events that they have created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704021130) do
+ActiveRecord::Schema.define(version: 20160704041120) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:'First', email:'first@example.com')
+User.create(name:'Second', email:'second@example.com')
+User.create(name:'Third', email:'third@example.com')
+User.create(name:'Fourth', email:'fourth@example.com')
+User.create(name:'Fifth', email:'fifth@example.com')
+
+# not fully correct, using ID, but just want to seed...
+Event.create(name: "First's party", location: "Edmonton", date:DateTime.now, user_id: 1)
+Event.create(name: "Seconds's party", location: "Toronto", date:DateTime.now, user_id: 2)
+Event.create(name: "Third's party", location: "Vancouver", date:DateTime.now, user_id: 3)
