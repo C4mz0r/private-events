136400b5d1e7d3ec5722b23452ad35f8fd17aaa9
C4mz0r <aut0x@hotmail.com>
2016-07-05 09:57:32 -0600

    Create a link to visit a random friend's home page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def selectRandomUser
+    randomUser = nil
+    loop do 
+      randomUser = User.all.sample
+      break if (randomUser != current_user)
+    end
+    randomUser
+  end
 end
